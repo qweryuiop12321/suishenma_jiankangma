16c7985a11530b3ef71e0a2fc26ba754ec248c40
xxx <xxxxx>
2022-11-06 18:39:06 +0800

    add new feature: custom addresses by user

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,70 +1,30 @@
 // pages/index_address/index_address.js
-import drawQrcode from '../../utils/weapp.qrcode.esm.js'
 var util_s = require('../../utils/util_s.js');
 const app = getApp();
 Page({
   data: {
-      title: "",
-      address: "", 
-      touchx: 0,
-      touchy: 0, 
-      default_address:true
-  }, 
+    title: "",
+    address: "",
+    touchx: 0,
+    touchy: 0,
+    default_address: true
+  },
 
   onLoad: function (options) {
-    // const query = wx.createSelectorQuery()
-    // query.select('#myQrcode')
-    // .fields({
-    //     node: true,
-    //     size: true
-    // })
-    // .exec((res) => {
-    //   var canvas = res[0].node
-    
-    //   // 调用方法drawQrcode生成二维码
-    //   drawQrcode({
-    //       canvas: canvas,
-    //       canvasId: 'myQrcode',
-    //       width: 200,
-    //       height:200,
-    //       padding: 30,
-    //       background: "#FFFFFF",
-    //       foreground: '#09BC68',
-    //       text: '个人二维码信息',
-
-    //   })
-    //   wx.canvasToTempFilePath({
-    //     canvasId: 'myQrcode',
-    //     canvas: canvas,
-    //     x: 0,
-    //     y: 0,
-    //     width: 200,
-    //     height: 200,
-    //     destWidth: 200,
-    //     destHeight: 200,
-    //     success(res) {
-    //         console.log('二维码临时路径：', res.tempFilePath)
-    //     },
-    //     fail(res) {
-    //         console.error(res)
-    //     }
-    //   })
-    // })
-
     var that = this;
     // get init brightness
     wx.getScreenBrightness({
-      success: function (res) {that.setData({ScreenBrightness: res.value})}
-    }) 
-    wx.setScreenBrightness({value: 1})
+      success: function (res) { that.setData({ ScreenBrightness: res.value }) }
+    })
+    wx.setScreenBrightness({ value: 1 })
     // update time every second
     setInterval(function () {
       that.setData({
         Time: util_s.formatTime(new Date()),
         Time_s: util_s.formatTime_s(new Date())
-      }); 
+      });
     }, 1000);
-  }, 
+  },
 
   // 记录触摸起点
   touchStart(e) {
@@ -83,17 +43,17 @@ Page({
     if (x - that.data.touchx > 50 && Math.abs(y - that.data.touchy) < 50) {      //右滑
       turn = "right"
     } else if (x - that.data.touchx < -50 && Math.abs(y - that.data.touchy) < 50) {   //左滑
-        turn="left";
+      turn = "left";
     }
     //根据方向进行操作
-    if(turn == 'left'){
-        wx.navigateTo({
-          url: '../index_adress/index_adress',
-        }) ; 
+    if (turn == 'left') {
+      wx.navigateTo({
+        url: '../index_adress/index_adress',
+      });
     }
-  }, 
+  },
   // set brightness to init
-  onUnload: function () { 
-    wx.setScreenBrightness({value: this.data.ScreenBrightness})
+  onUnload: function () {
+    wx.setScreenBrightness({ value: this.data.ScreenBrightness })
   },
 })
--- pages/index_adress/index_adress.js
+++ pages/index_adress/index_adress.js
@@ -1,46 +1,123 @@
- // pages/index_address/index_address.js
+// pages/index_address/index_address.js 
 var util_s = require('../../utils/util_s.js');
-const app = getApp();
+const app = getApp()
+
 Page({
-  data:{
-      title: "",
-      address: "",
+  data: {
+    title_list: ['', '', '', ''],
+    address_list: ['', '', '', ''],
+    checked_address: -1,
   },
 
-  input_handler:function(e){
-    console.log(e.target.id)
-    console.log(e.detail.value)
+  ischecked_func: function ({ currentTarget }) {
     this.setData({
-        [e.target.id] : e.detail.value,
+      checked_address: currentTarget.id,
+    })
+  },
+  input_handler: function (e) {
+    if (e.target.id == "title") {
+      switch (this.data.checked_address) {
+        case '0':
+          this.setData({
+            ["title_list[0]"]: e.detail.value,
+          }); break;
+        case '1':
+          this.setData({
+            ["title_list[1]"]: e.detail.value,
+          }); break;
+        case '2':
+          this.setData({
+            ["title_list[2]"]: e.detail.value,
+          }); break;
+        case '3':
+          this.setData({
+            ["title_list[3]"]: e.detail.value,
+          });
+          break;
+      }
+    }
+    if (e.target.id == "address") {
+      switch (this.data.checked_address) {
+        case '0':
+          this.setData({
+            ["address_list[0]"]: e.detail.value,
+          }); break;
+        case '1':
+          this.setData({
+            ["address_list[1]"]: e.detail.value,
+          }); break;
+        case '2':
+          this.setData({
+            ["address_list[2]"]: e.detail.value,
+          }); break;
+        case '3':
+          this.setData({
+            ["address_list[3]"]: e.detail.value,
+          }); break;
+      }
+    }
+  },
+
+  save_address: function (e) {
+    var pages = getCurrentPages();
+    var currPage = pages[pages.length - 1];   //当前页面
+    var prevPage = pages[pages.length - 2];  //上一个页面
+    if (this.data.checked_address == -1) {
+      return
+    }
+    prevPage.setData({
+      ["title"]: this.data.title_list[this.data.checked_address],
+      ["address"]: this.data.address_list[this.data.checked_address],
+      ["default_address"]: false
+    })
+    wx.navigateBack({
+      delta: 1,
     })
   },
 
-  save_address:function(e){
-      var pages = getCurrentPages();
-      var currPage = pages[pages.length - 1];   //当前页面
-      var prevPage = pages[pages.length - 2];  //上一个页面
-      if (this.data.title==""){
-        this.data.title="通用"
+  exit: function (e) {
+    wx.navigateBack({
+      delta: 1,
+    })
+  },
+  onLoad: function (query) {
+    try {
+      const res = wx.getStorageInfoSync()
+      if (res.keys.length > 0) {
+        this.setData({
+          title_list: wx.getStorageSync('title_list'),
+          address_list: wx.getStorageSync('address_list'),
+        })
+      } else {
+        this.setData({
+          title_list: ['', '', '', ''],
+          address_list: ['', '', '', ''],
+        })
+        console.log("No cache address data.")
       }
-      if (this.data.address==""){
-        this.data.address="上海市/徐汇区/龙华路3166弄"
-      }
-      prevPage.setData({
-          ["title"]:this.data.title,
-          ["address"]:this.data.address,
-          ["default_address"]:false
-      })
-      wx.navigateBack({
-        delta: 1,
-      })
+    } catch (e) {
+      console.log("sotrage load error!")
+    };
+    var pages = getCurrentPages();
+    var currPage = pages[pages.length - 1];   //当前页面
+    var prevPage = pages[pages.length - 2];  //上一个页面
+    prevPage.setData({
+      ["default_address"]: true,
+    })
+  },
+  onUnload: function () {
+    wx.setStorage({
+      key: "title_list",
+      data: this.data.title_list,
+    })
+    wx.setStorage({
+      key: "address_list",
+      data: this.data.address_list,
+    })
+
+    this.data.require
   },
 
-  exit:function(e){
-      wx.navigateBack({
-        delta: 1,
-      })
-  }
 })
 
 
- 
